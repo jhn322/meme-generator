9738d18c9eb89157599932b08c83c0964bf916c7
Johan Söderlund <johan.soderlund96@gmail.com>
2024-03-25 00:25:28 +0100

    array with memes

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,13 +1,34 @@
 import React from "react";
+import MemeData from "./MemeData";
 
 const Meme = () => {
+  function getMemeImage() {
+    const memeArray = MemeData.data.memes;
+    const randumNumber = Math.floor(Math.random() * memeArray.length);
+    url = memeArray[randumNumber].url;
+    console.log(url);
+  }
+
   return (
     <main>
-      <form className="meme-form">
-        <input className="meme-input" type="text" placeholder="Top text" />
-        <input className="meme-input" type="text" placeholder="Bottom text" />
-        <button className="meme-button">Generate new meme</button>
-      </form>
+      <div className="meme-form">
+        {" "}
+        <input
+          id="first input"
+          className="meme-input"
+          type="text"
+          placeholder="Kappa"
+        />{" "}
+        <input
+          id="second input"
+          className="meme-input"
+          type="text"
+          placeholder="Ross"
+        />
+        <button id="button" onClick={getMemeImage} className="meme-button">
+          Generate new meme
+        </button>
+      </div>
     </main>
   );
 };
